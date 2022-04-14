3fb5fcaf89994d45a340054ae24616f3ce45d4e7
g28890aa <andrei.aleksanian@student.manchester.ac.uk>
2022-04-14 17:08:10 +0100

    Try grid search

--- src/lstm/model.py
+++ src/lstm/model.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+from sklearn.model_selection import GridSearchCV
+from keras.wrappers.scikit_learn import KerasRegressor
 
 LEARNING_RATE = 0.001
 
@@ -13,3 +15,16 @@ def getModel(inputs):
       learning_rate=LEARNING_RATE), loss="mse", metrics=['acc'])
 
   return model
+
+
+def getWrappedModel(inputs):
+  return KerasRegressor(build_fn=getModel, inputs=inputs)
+
+
+def getGrid(inputs):
+  model = getWrappedModel(inputs)
+  batch_size = [10, 20, 40, 60, 80, 100]
+  epochs = [10, 50, 100]
+  param_grid = dict(batch_size=batch_size, epochs=epochs)
+  grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1, cv=5)
+  return grid
